fc0cd47c823b3df525a9c24c28b69057fcf57001
Kent Duncan <mr_reality15@hotmail.com>
2011-12-01 16:54:19 -0700

    Commit

--- PUPs/ui_mainwindow.h
+++ PUPs/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Thu Dec 1 15:31:33 2011
+** Created: Thu Dec 1 16:52:40 2011
 **      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
